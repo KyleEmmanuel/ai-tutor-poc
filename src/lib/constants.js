4c35d3aae52e792ec171e83fa6ecdfdf3cf1bfd2
KyleEmmanuel <ochoa.kyleemmanuel.f.8736@gmail.com>
2025-01-08 17:33:33 +0800

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://ai-tutor-api.discflow.org/api'
+export const API_URL = 'https://ai-tutor-api.discflow.org/api'
 export const plain_text_assistant_id = 'asst_899vDhCJAWb8s4KU3PW9O0kQ'
 export const json_assistant_id = 'asst_zI6nygO9cX4PSHvRnkp3i3k2'
